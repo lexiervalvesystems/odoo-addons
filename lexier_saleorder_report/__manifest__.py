dfc89651df4953a5b39cab212f80a9de362a26e9
Eric Antones <eantones@nuobit.com>
2022-02-22 19:24:53 +0100

    [ADD] lexier_saleorder_report: new module with Lexier sale order customizations

---
+++ lexier_saleorder_report/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Lexier Sale Order report",
+    "summary": "Lexier Sale Order report",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/lexiersolutions/odoo-addons",
+    "depends": ["sale"],
+    "data": [
+        "views/report_saleorder_document.xml",
+    ],
+    "installable": True,
+}
