b89e26a831025fc9131a46e6eba3f36899a3ea40
Eric Antones <eantones@nuobit.com>
2023-01-05 00:25:51 +0100

    [ADD] lexier_invoice_report: new module with Lexier invoice customizations

---
+++ lexier_invoice_report/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Lexier Invoice report",
+    "summary": "Lexier Invoice report",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/lexiersolutions/odoo-addons",
+    "depends": ["account"],
+    "data": [
+        "views/report_invoice_document.xml",
+    ],
+}
