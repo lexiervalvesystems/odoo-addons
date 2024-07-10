c0bcb1b28471636569ed8cb8dc0c096e0f72b224
FrankC013 <fcespedes@nuobit.com>
2024-07-10 18:04:42 +0200

    [ADD] lexier_purchaseorder_report

---
+++ lexier_purchaseorder_report/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Lexier Purchase Order report",
+    "summary": "Lexier Purchase Order report",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Inventory/Purchase",
+    "website": "https://github.com/lexiervalvesystems/odoo-addons",
+    "depends": ["purchase_stock"],
+    "data": [
+        "views/report_purchaseorder_document.xml",
+    ],
+}
