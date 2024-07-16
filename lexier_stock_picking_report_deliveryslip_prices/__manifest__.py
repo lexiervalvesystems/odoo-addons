df8803bf831c9cb24b3aa904ba9c3a4fd116bcae
FrankC013 <fcespedes@nuobit.com>
2024-07-16 14:53:46 +0200

    [ADD] lexier_stock_picking_report_deliveryslip_prices

---
+++ lexier_stock_picking_report_deliveryslip_prices/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Lexier Stock Picking Report Delivery Slip Prices",
+    "summary": "Lexier Stock Picking Report Delivery Slip with Prices",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Warehouse",
+    "website": "https://github.com/lexiervalvesystems/odoo-addons",
+    "depends": ["lexier_stock_picking_report_deliveryslip"],
+    "data": [
+        "views/report_deliveryslip_with_prices.xml",
+        "report/report.xml",
+    ],
+}
