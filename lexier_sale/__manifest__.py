f10881e2e0874c8e3b39a5df3e099073a452882b
FrankC013 <fcespedes@nuobit.com>
2024-07-19 15:35:12 +0200

    [ADD] lexier_sale

---
+++ lexier_sale/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Lexier Sale",
+    "summary": "This module adds specific customizations for Lexier in the Sales module.",
+    "version": "14.0.0.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Sales/Sales",
+    "website": "https://github.com/lexiervalvesystems/odoo-addons",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+}
---
+++ lexier_sale/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds specific customizations for Lexier in the Sales module.
